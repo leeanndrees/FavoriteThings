604bf544bc898b9b366381d53f8f51b5342e99cd
Leeann Drees <leeann.drees@detroitlabs.com>
2018-08-27 16:30:25 -0400

    adding first basic methods for getting # of rows and constructing cells

--- FavoriteThings/FavoriteThingsViewController.swift
+++ FavoriteThings/FavoriteThingsViewController.swift
@@ -20,6 +20,15 @@ class FavoriteThingsViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteThingsItem", for: indexPath)
+        
+        return cell
+    }
 
 }
 
