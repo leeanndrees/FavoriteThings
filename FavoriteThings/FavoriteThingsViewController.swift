b9957f5a4cf85b734a8867669732bc65ee005d38
Leeann Drees <leeann.drees@detroitlabs.com>
2018-08-27 16:19:10 -0400

    initial commit

---
+++ FavoriteThings/FavoriteThingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavoriteThings
+//
+//  Created by DetroitLabs on 8/27/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteThingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
